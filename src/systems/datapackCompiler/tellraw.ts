bf86fbb330f0c8256acf7156eb3888088280a777
SnaveSutit <SnaveSutit@gmail.com>
2026-06-23 21:04:06 -0400

    🐛 Fix broken website links

--- src/systems/datapackCompiler/tellraw.ts
+++ src/systems/datapackCompiler/tellraw.ts
@@ -174,7 +174,7 @@ namespace TELLRAW {
 				],
 			},
 			CREATE_TELLRAW_HELP_LINK(
-				'https://animated-java.dev/docs/rigs/controlling-a-rig-instance'
+				'https://animated-java.dev/docs/core-concepts/tags#practical-selector-examples'
 			),
 		])
 	}
