e5165cfb12e8d73952cfde3e2fe5c8c3a3f7e894
SnaveSutit <SnaveSutit@gmail.com>
2026-06-24 11:56:39 -0400

    🐛 Fix outdated rig warning

--- src/systems/datapackCompiler/tellraw.ts
+++ src/systems/datapackCompiler/tellraw.ts
@@ -1,4 +1,5 @@
 import { TextComponent, type TextElement } from 'book-and-quill'
+import { projectTargetVersionIsAtLeast } from '../../formats/blueprint'
 import { toSmallCaps } from '../../util/minecraftUtil'
 import { type IRenderedAnimation } from '../animationRenderer'
 import { type IRenderedVariant } from '../rigRenderer'
@@ -123,15 +124,21 @@ namespace TELLRAW {
 					},
 		])
 
-	export const RIG_OUTDATED_TEXT_DISPLAY = () =>
-		renderTextComponent(
+	export const RIG_OUTDATED_TEXT_DISPLAY = () => {
+		// upstream は TextComponent.toString() を使うが、 1.20.4 で SNBT 化される問題を避けるため
+		// fork 側の renderTextComponent (JSON.stringify 経路) を維持する。
+		let text = renderTextComponent(
 			new TextComponent([
 				{ text: '⚠ This rig instance is outdated! ⚠', color: 'red' },
 				'\n It should be removed and re-summoned to ensure it functions correctly.',
 			])
 		)
-			// Because this is used as NBT in a summon command, we need to double-escape the newlines.
-			.replaceAll('\\n', '\\\\n')
+		if (!projectTargetVersionIsAtLeast('1.21.5')) {
+			// Because this is used as an NBT string in 1.21.4 and below, we need to double-escape the newlines.
+			text = text.replaceAll('\\n', '\\\\n')
+		}
+		return text
+	}
 
 	export const FUNCTION_NOT_EXECUTED_AS_ROOT_ERROR = (functionPath: string, tag: string) => {
 		const hoverText = new TextComponent([
